715d644a5aa57e08bcf7f7d92873e9f573759bae
Gil Gruber <gilgru54@gmail.com>
2022-06-11 16:06:28 +0300

    adding untracked files

---
+++ Client/src/model/feedback.js
@@ -0,0 +1,8 @@
+export class Feedback {
+  constructor(dosingId, time, rating, comment) {
+    this.dosingId = dosingId
+    this.time = time
+    this.rating = rating
+    this.comment = comment
+  }
+}
