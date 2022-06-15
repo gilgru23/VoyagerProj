2f1e3745092151afd0062a3cfb1450e1662cc20b
magrils <elp1234l>
2022-06-15 16:26:50 +0300

    changes to admin web database

--- django_site/consumer_api/admin.py
+++ django_site/consumer_api/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from .models import Consumer, Dispenser
+from .models import Consumer, Dispenser, Business, Representative
+
+from .models import *
 
 # Register your models here.
 admin.site.register(Consumer)
 admin.site.register(Dispenser)
+admin.site.register(Business)
+admin.site.register(Representative)
+admin.site.register(Company)
+admin.site.register(PodType)
+admin.site.register(Pod)
+admin.site.register(Regimen)
+admin.site.register(Dosing)
+admin.site.register(Feedback)
+admin.site.register(FeedbackReminder)
+admin.site.register(Caregiver)
